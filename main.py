d46b9e152fc1fd0f63d596bb430f91918c9fea8b
Timon Ludwig <timon-ludwig@gmx.de>
2026-04-20 14:15:58 +0200

    small change translate_yt timestamp

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import yt_dlp
 import requests
 import whisper 
 import os 
+import sys
 
 model = whisper.load_model("medium")
 
@@ -25,17 +26,21 @@ def translate_ytsubtitle(info):
             parts = l.split("\n")
             if len(parts) >= 3:
                 timestamp.append({"timestamp":parts[1], "text":parts[2]})
+        text = ""
         for t in timestamp:
-            print(f"{t["timestamp"]}: \n{translate(t["text"])}\n")
+            text = text + t
+            #print(f"{t["timestamp"]}: \n{translate(t["text"])}\n")
+        translated_full = translate(text)
+        print(translated_full)
 
 def smart_translate(url):
     with yt_dlp.YoutubeDL() as ydl:
         info = ydl.extract_info(url, download=False)   #dict mit ALLEN Video-infos (Titel, Dauer, ...) - auch mit "automatic_captions"
         if "ko" in info["subtitles"] or "ko" in info["automatic_captions"]:
-            print("Untertitel gefunden, starte Übersetzung")
+            print("\nUntertitel gefunden, starte Übersetzung\n")
             translate_ytsubtitle(info)
         else:
-            print("Keine Untertitel gefunden, verwende AI um Audio zu übersetzen")
+            print("\nKeine Untertitel gefunden, verwende AI um Audio zu übersetzen\n")
             translate_audio(url)
 
 def download_audio(link):
@@ -77,7 +82,8 @@ def select():
         else:
             print("Keine gültige Eingabe")
 
-select()
+if __name__ == "__main__":
+    select()
 
 
 
